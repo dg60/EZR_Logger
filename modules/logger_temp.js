6e44b4a67e982b5f9c443a431b8ac47b1beed91d
Daniel Grill <dgrill60@gmail.com>
2018-01-13 09:01:08 +0100

    improve error handling. After some connection erros from the pm2 log.

--- modules/logger_temp.js
+++ modules/logger_temp.js
@@ -46,7 +46,22 @@ module.exports = {
             headers: { 'Cache-Control': 'no-cache' },
             agent: false  // create a new agent just for this one request
         }, (res) => {
+        
+            const {statusCode} = res;
+            let error;
 
+            if (statusCode !== 200) {
+            error = new Error('Request Failed.\n' +
+                                  `Status Code: ${statusCode}`);
+            }
+              
+            if (error) {
+                console.error(error.message);
+                // consume response data to free up memory
+                res.resume();
+                return;
+            }
+            
             res.on('data', function (chunk) {
                 output += chunk;
                 xml = output;
@@ -87,6 +102,8 @@ module.exports = {
                 });
 
             });
+        }).on('error',(e) =>{
+           console.error('Got error: ${e.message}'); 
         });
 
         // Save the data
